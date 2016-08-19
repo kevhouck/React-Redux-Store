2298f169c55c780ff9745d03b13542523f058383
kevhouck <kevin.houck.95@gmail.com>
2016-08-19 16:15:14 -0500

    Add Bootstrap and jQuery to webapp

--- webapp/src/js/app.js
+++ webapp/src/js/app.js
@@ -1,5 +1,8 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+// cannot use bootstrap without polluting the global scope unfortunately
+window.$ = window.jQuery = require('jquery');
+require('bootstrap');
 
 var App = React.createClass({
     render: function() {
