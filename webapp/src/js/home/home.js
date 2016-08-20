2bf6bdb009560dcc6e05b2a5cdb41bfd532d0baf
kevhouck <kevin.houck.95@gmail.com>
2016-08-20 13:03:24 -0500

    Add sample content to home page

--- webapp/src/js/home/home.js
+++ webapp/src/js/home/home.js
@@ -1,9 +1,26 @@
 var React = require('react');
+var Grid = require('react-bootstrap').Grid;
+var Row = require('react-bootstrap').Row;
+var Col = require('react-bootstrap').Col;
+
+var PageHeader = require('react-bootstrap').PageHeader;
 
 var Home = React.createClass({
     render: function () {
         return (
-            <div>Home</div>
+            <div>
+                <Grid>
+                    <Row>
+                        <Col>
+                            <PageHeader>Lexie's Cupcakes <small>Mmmmmmmmmmm</small></PageHeader>
+                            <div>
+                                <p>asdf asdfasdf asd fasdf a f a a ds as dfadf adfasdfasdf adfasdf asdasdf</p>
+                                <img src="http://www.browneyedbaker.com/wp-content/uploads/2010/11/red-velvet-cupcakes-32-600.jpg" width={600} height={400}/>
+                            </div>
+                        </Col>
+                    </Row>
+                </Grid>
+            </div>
         )
     }
 });
