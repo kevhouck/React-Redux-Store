e7d4fb8bb99ad9d0bcddec3fe92f12280a1121e1
kevhouck <kevin.houck.95@gmail.com>
2016-08-20 22:32:40 -0500

    Add collapsing navbar for small screens

--- webapp/src/js/app/header.js
+++ webapp/src/js/app/header.js
@@ -13,13 +13,16 @@ var Header = React.createClass({
                         <Navbar.Brand>
                             Lexie's Cupcakes
                         </Navbar.Brand>
+                        <Navbar.Toggle />
                     </Navbar.Header>
-                    <Nav>
-                        <LinkContainer to={{pathname :'/home' }}><NavItem>Home</NavItem></LinkContainer>
-                        <LinkContainer to={{pathname :'/blog' }}><NavItem>Blog</NavItem></LinkContainer>
-                        <LinkContainer to={{pathname :'/store' }}><NavItem>Store</NavItem></LinkContainer>
-                        <LinkContainer to={{pathname :'/about' }}><NavItem>About</NavItem></LinkContainer>
-                    </Nav>
+                    <Navbar.Collapse>
+                        <Nav>
+                            <LinkContainer to={{pathname :'/home' }}><NavItem eventKey={1}>Home</NavItem></LinkContainer>
+                            <LinkContainer to={{pathname :'/blog' }}><NavItem eventKey={2}>Blog</NavItem></LinkContainer>
+                            <LinkContainer to={{pathname :'/store' }}><NavItem eventKey={3}>Store</NavItem></LinkContainer>
+                            <LinkContainer to={{pathname :'/about' }}><NavItem eventKey={4}>About</NavItem></LinkContainer>
+                        </Nav>
+                    </Navbar.Collapse>
                 </Navbar>
             </div>
         )
