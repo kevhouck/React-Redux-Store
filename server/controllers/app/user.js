fdc9968b4a71d8626e2e0eb80b539eeafd42bc0d
kevhouck <kevin.houck.95@gmail.com>
2016-08-24 17:26:56 -0500

    Change user controllers to conform to standard and webapp auth logic

--- server/controllers/app/user.js
+++ server/controllers/app/user.js
@@ -5,29 +5,38 @@ var bcrypt = require('bcrypt');
 var signup = function (req, res) {
     var b = req.body;
 
-    if (b.password) {
-        bcrypt.hash(b.password, 12, function (err, hashedPassword) {
-            if (err) {
-                res.statusCode(500);
-                return;
-            }
-
-            var newUser = new User({
-                firstName: b.firstName,
-                lastName: b.lastName,
-                hashedPassword: hashedPassword,
-                createdOn: new Date(),
-                emailAddress: b.emailAddress
-            });
-            newUser.save().then(function (user) {
-                delete user.hashedPassword;
-                res.send(user);
-            }).catch(function (err) {
-                if (err) {
-                    res.status(400).send(err);
-                }});
-        })
+    if(!b || !b.password) {
+        res.sendStatus(400)
+        return;
     }
+
+    bcrypt.hash(b.password, 12, function (err, hashedPassword) {
+        if (err) {
+            res.sendStatus(500)
+            return;
+        }
+
+        var newUser = new User({
+            firstName: b.firstName,
+            lastName: b.lastName,
+            hashedPassword: hashedPassword,
+            createdOn: new Date(),
+            emailAddress: b.emailAddress
+        });
+        newUser.save().then(function (user) {
+            jwt.sign({ user: user.emailAddress, authLevel: user.authLevel }, 'secret', { algorithm: 'HS512' }, function (err, token) {
+                var resObj = {
+                    user: user,
+                    credentials: token
+                };
+                res.send(resObj);
+            });
+        }).catch(function (err) {
+            if (err) {
+                res.status(400).send(err);
+            }});
+    })
+
 };
 
 var login = function (req, res) {
@@ -39,14 +48,13 @@ var login = function (req, res) {
             }
             bcrypt.compare(b.password, user.hashedPassword, function (err, result) {
                 if (err) {
-                    res.statusCode(500);
+                    res.status(500).send();
                 }
                 if (result) {
                     jwt.sign({ user: user.emailAddress, authLevel: user.authLevel }, 'secret', { algorithm: 'HS512' }, function (err, token) {
-                        delete user.hashedPassword;
                         var resObj = {
                             user: user,
-                            jwt: token
+                            credentials: token
                         };
                         res.send(resObj);
                     });
